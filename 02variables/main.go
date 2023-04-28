49639e19fe162009e407f2f249848d56be3ae000
VimVek <vimvekjomshi@gmail.com>
2023-04-28 16:17:01 +0530

    GolangBasic

---
+++ 02variables/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// constant
+const LoginToken string = "hiIamConstant" //capital first character i.e, LoginToken states that this is Public variable
+
+func main() {
+	var username string = "Vivek" // we have to use the variable is we make it
+	fmt.Println("username")       //fp short form to for fmt.Println
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = true // type boolean
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255 // untyped integer 0-255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.12341341313 // untyped integer 0-255
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// Default value and some aliases
+	var anotherVar int
+	var anotherStn string
+	fmt.Println(anotherVar)
+	fmt.Println(anotherStn)
+	fmt.Printf("Variable is of type: %T \n", anotherVar)
+	fmt.Printf("Variable is of type: %T \n", anotherStn)
+
+	//implicit type (it auto assign type according to value but we cant change it later on)
+	var website = "hieloo.in"
+	fmt.Println(website)
+	//cant be change to website = 3 as it is string
+
+	// no var style (total ignore keyword var and still declare variable)
+	numberOfUser := 300000 // := is walrus operator allowed inside method only, not allowed outside
+	fmt.Println(numberOfUser)
+
+	//const
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+
+}
---
+++ 03userinput/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	welcome := "Welcome user input"
+	fmt.Println(welcome)
+}
